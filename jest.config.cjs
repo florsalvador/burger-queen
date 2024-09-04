f6720077276231eaa2a3b5d6585e9f0df1024f44
florsalvador <florsalvador219@gmail.com>
2024-09-04 02:41:38 -0500

    Setup React project and add testing libraries

---
+++ jest.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  roots: ["<rootDir>/src"],
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.(ts|js|tsx|jsx)$": "@swc/jest",
+  },
+  transformIgnorePatterns: [
+    "[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs|cjs|ts|tsx)$",
+  ],
+  moduleNameMapper: {
+    "^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy",
+    "^.+\\.(css|png|jpg|jpeg|svg)$": "<rootDir>/src/__tests__/__mocks__/fileMock.cjs",
+  },
+};
