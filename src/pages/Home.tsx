d3a04637d48a1d53c51d8b4a9e25866ef9f25b4c
florsalvador <florsalvador219@gmail.com>
2024-09-13 16:18:14 -0500

    feat: Add ProductList and TakeOrder components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,65 @@
 import { useNavigate } from "react-router-dom";
-import { clearSession } from "../services/authService";
+import { useEffect, useState } from "react";
+import { getSession, clearSession } from "../services/authService";
+import { getProducts } from "../services/productService";
+import Product from "../models/Product";
+import ProductList from "../components/ProductList";
+import TakeOrder from "../components/TakeOrder";
+import "../styles/Home.css";
 
 function Home() {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [error, setError] = useState(false);
+  const [productsToAdd, setProductsToAdd] = useState<{ qty: number, product: Product }[]>([]);
   const navigate = useNavigate();
   const handleLogout = () => {
     clearSession();
     navigate("/login");
   };
 
+  useEffect(() => {
+    getProducts()
+    .then(data => setProducts(data))
+    .catch(error => {
+      console.error(error);
+      setError(true);
+    })
+  }, []);
+
+  const session = getSession();
+
+  const addProducts = (qty: number, product: Product) => {
+    setProductsToAdd(prevProducts => {
+      const existingProduct = prevProducts.find(p => p.product.id === product.id);
+      if (existingProduct) {
+        const updatedProducts = prevProducts.map(p => 
+          p.product.id === product.id ? { ...p, qty: p.qty + qty } : p
+        );
+        console.log("Updated existing product:", updatedProducts);
+        return updatedProducts;
+      } else {
+        const newProducts = [...prevProducts, { qty, product }];
+        console.log("Added new product:", newProducts);
+        return newProducts;
+      }
+    });
+  };
+
   return (
     <>
-      <h1>Home</h1>
-      <button data-testid="logoutBtn" onClick={handleLogout}>Logout</button>
+      <header>
+        <h1>BurgerQueen</h1>
+        <p>Welcome {session.user?.role}</p>
+      </header>
+      <main>
+        <div>
+          <a href="#">Menu</a>
+          <a href="#">Order List</a>
+          <button data-testid="logoutBtn" onClick={handleLogout}>Logout</button>
+        </div>
+        {!error && <ProductList products={products} onAdd={addProducts} />}
+        <TakeOrder productsToAdd={productsToAdd} setProductsToAdd={setProductsToAdd} />
+      </main>
     </>
   );
 }
