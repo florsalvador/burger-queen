50c26c20b1daadf2ce523887c70d9c21f37eb765
florsalvador <florsalvador219@gmail.com>
2024-09-13 22:53:44 -0500

    feat: Add menu filter for breakfast and lunch products

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import ProductCard from "./ProductCard";
 import Product from "../models/Product";
 import styles from "../styles/ProductList.module.css";
@@ -8,13 +9,42 @@ interface ProductListProps {
 }
 
 function ProductList({ products, onAdd } : ProductListProps) {
-  const productList = products.map(product =>
+  const [ type, setType ] = useState<string>("all");
+  const [ filteredProducts, setFilteredProducts ] = useState<Product[]>(products);
+
+  useEffect(() => {
+    if (type == "breakfast") {
+      const breakfastList = products.filter(p => p.type=="Breakfast" || p.name.includes("coffee") || p.name=="Milk");
+      setFilteredProducts(breakfastList);
+    } else if (type == "lunch") {
+      const lunchList = products.filter(p => !(p.type=="Breakfast" || p.name.includes("coffee") || p.name=="Milk"));
+      setFilteredProducts(lunchList);
+    } else setFilteredProducts(products);
+  }, [products, type]);
+
+  const productList = filteredProducts.map(product =>
     <ProductCard key={product.id} product={product} onAdd={onAdd} />
   )
 
   return (
-    <div className={styles.productList}>
-      {productList}
+    <div>
+      <div>
+        <label>
+          <input type="radio" name="filter" value="all" onChange={(e) => setType(e.target.value)} defaultChecked />
+          All
+        </label>
+        <label>
+          <input type="radio" name="filter" value="breakfast" onChange={(e) => setType(e.target.value)} />
+          Breakfast
+        </label>
+        <label>
+          <input type="radio" name="filter" value="lunch" onChange={(e) => setType(e.target.value)} />
+          Lunch
+        </label>
+      </div>
+      <div className={styles.productList}>
+        {productList}
+      </div>
     </div>
   );
 }
