847a01a6734fb306b6bbee488d986b0858dc8e1b
florsalvador <florsalvador219@gmail.com>
2024-09-25 22:19:50 -0500

    feat: Add styles for Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,6 @@ import { getProducts } from "../services/productService";
 import Product from "../models/Product";
 import ProductList from "../components/ProductList";
 import TakeOrder from "../components/TakeOrder";
-import "../styles/Home.css";
 
 function Home() {
   const [products, setProducts] = useState<Product[]>([]);
@@ -46,21 +45,27 @@ function Home() {
   };
 
   return (
-    <>
-      <header>
-        <h1>BurgerQueen</h1>
-        <p>Welcome {session.user?.role}</p>
+    <div className="h-screen flex flex-col">
+      <header className="flex justify-between p-4 border-b-[1px] border-gray-400 box-border">
+        <h1 className="text-2xl font-bold">
+          🍔 <span className="text-gray-500">Burger</span><span className="text-amber-500">Queen</span>
+        </h1>
+        <p className="self-end">Welcome {session.user?.role}</p>
       </header>
-      <main>
-        <div>
-          <a href="#">Menu</a>
-          <a href="#">Order List</a>
-          <button data-testid="logoutBtn" onClick={handleLogout}>Logout</button>
+      <main className="grid grid-cols-[15%,85%] h-full">
+        <div className="pb-24 flex flex-col justify-between bg-gray-100 font-semibold text-gray-600">
+          <div className="pt-4">
+            <a className="block bg-white py-3 pl-6" href="#">Menu</a>
+            <a className="block bg-gray-100 py-3 pl-6" href="#">Order List</a>
+          </div>
+          <button data-testid="logoutBtn" onClick={handleLogout}>{"<"} Log out</button>
         </div>
-        {!error && <ProductList products={products} onAdd={addProducts} />}
-        <TakeOrder productsToAdd={productsToAdd} setProductsToAdd={setProductsToAdd} />
+        <div className="p-4 grid grid-cols-[68%,32%] gap-4 h-full overflow-y-auto">
+          {!error && <ProductList products={products} onAdd={addProducts} />}
+          <TakeOrder productsToAdd={productsToAdd} setProductsToAdd={setProductsToAdd} />
+        </div>
       </main>
-    </>
+    </div>
   );
 }
 
