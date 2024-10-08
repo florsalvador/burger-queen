7144aa504fde5255fa51635f7eb3cceb3226e4d6
florsalvador <florsalvador219@gmail.com>
2024-10-08 10:07:00 -0700

    Add getOrders and modifyOrder functions

--- src/services/orderService.ts
+++ src/services/orderService.ts
@@ -18,3 +18,35 @@ export async function createOrder(newOrder: NewOrder): Promise<Order> {
   const orderInfo = await response.json();
   return orderInfo;
 }
+
+export async function getOrders(): Promise<Order[]> {
+  const session = getSession();
+  const response = await fetch(`${API_URL}/orders`, {
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${session.token}`
+    },
+  });
+  if (!response.ok) {
+    throw new Error("Error getting orders");
+  }
+  const data = await response.json();
+  return data;
+}
+
+export async function modifyOrder(id: number, status: string): Promise<Order> {
+  const session = getSession();
+  const response = await fetch(`${API_URL}/orders/${id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${session.token}`
+    },
+    body: JSON.stringify({ "status": status }),
+  });
+  if (!response.ok) {
+    throw new Error("Error modifying order");
+  }
+  const orderInfo = await response.json();
+  return orderInfo;
+}
