53c55ab9d9d2b718b4596b9fb2c8484db0550f38
florsalvador <florsalvador219@gmail.com>
2025-09-02 19:46:54 -0700

    feat(productlist): change font size of titles

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -21,14 +21,14 @@ function ProductList({ products, onAdd } : ProductListProps) {
       setFilteredProducts(lunchList);
     } else setFilteredProducts(products);
   }, [products, type]);
-
+  
   const productList = filteredProducts.map(product =>
     <ProductCard key={product.id} product={product} onAdd={onAdd} />
   )
 
   return (
     <div>
-      <h2 className="text-lg font-bold text-gray-700 pb-2">Select category</h2>
+      <h2 className="text-base font-bold text-gray-700 pb-2">Select category</h2>
       <div className={styles.filter}>
         <input type="radio" name="filter" id="all" value="all" onChange={(e) => setType(e.target.value)} defaultChecked />
         <label htmlFor="all">All</label>
@@ -37,7 +37,7 @@ function ProductList({ products, onAdd } : ProductListProps) {
         <input type="radio" name="filter" id="lunch" value="lunch" onChange={(e) => setType(e.target.value)} />
         <label htmlFor="lunch">Lunch</label>
       </div>
-      <h2 className="text-lg font-bold text-gray-700 pb-2">Select items</h2>
+      <h2 className="text-base font-bold text-gray-700 pb-2">Select items</h2>
       <div className="grid grid-cols-3 gap-[0.6rem] xl:grid-cols-4">
         {productList}
       </div>
