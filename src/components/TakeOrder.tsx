7d052dec7033d504c70b25b9499f98dfb95203c2
florsalvador <florsalvador219@gmail.com>
2025-09-02 19:57:18 -0700

    feat(takeorder): show spinner and error message on order creation

--- src/components/TakeOrder.tsx
+++ src/components/TakeOrder.tsx
@@ -14,6 +14,8 @@ function TakeOrder({ productsToAdd, setProductsToAdd } : TakeOrderProps) {
   const [ client, setClient ] = useState<string>("");
   const [ total, setTotal ] = useState<number>(0);
   const [ alert, setAlert ] = useState(false);
+  const [ error, setError ] = useState(false);
+  const [ loading, setLoading ] = useState(false);
   const session = getSession();
   const userId = session.user?.id ?? null;
   const order: NewOrder = {
@@ -30,6 +32,7 @@ function TakeOrder({ productsToAdd, setProductsToAdd } : TakeOrderProps) {
   }, [productsToAdd]);
 
   const handleCreateOrder = () => {
+    setLoading(true);
     createOrder(order)
       .then(response => {
         console.log("Order created successfully:", response);
@@ -41,8 +44,12 @@ function TakeOrder({ productsToAdd, setProductsToAdd } : TakeOrderProps) {
         }, 2000);
       })
       .catch(error => {
+        setError(true);
         console.error(error);
-      });
+      })
+      .finally(() => {
+        setLoading(false);
+      })
   };
 
   const updateSubtotal = (qty: number, id: number) => {
@@ -70,24 +77,64 @@ function TakeOrder({ productsToAdd, setProductsToAdd } : TakeOrderProps) {
   );
 
   return (
-    <div className="p-4 bg-gray-100 mr-4">
-      <h2 className="text-xl font-bold text-gray-700 pb-2">New Order</h2>
+    <div className="p-4 bg-gray-100">
+      <h2 className="text-base font-bold text-gray-700 pb-2">New Order</h2>
       {productsTotal}
-      {!alert && 
-      <>
-        <div className="flex justify-between font-semibold text-lg border-y border-dashed border-gray-600 py-1 my-5">
-          <span>Total:</span><span>${total}</span>
+      {loading ? (
+        <div className="flex justify-center items-center col-span-full">
+          <div
+            className="
+              animate-spin 
+              inline-block 
+              w-8 h-8 
+              border-4 
+              border-current 
+              border-t-transparent 
+              text-blue-500 
+              rounded-full
+            "
+            role="status"
+            aria-label="loading"
+          >
+            <span className="sr-only">Loading...</span>
+          </div>
         </div>
-        <div>
-          <input className="w-full border rounded placeholder:italic p-1 mb-4" type="text" id="customer" value={client} placeholder="Customer's name" onChange={e => setClient(e.target.value)} />
+      ) : error ? (
+        <div className="bg-rose-100 text-sm text-rose-700 py-3 px-4 rounded mb-3 text-center mt-3">
+          <p>Something went wrong</p>
+          <p>Please try again.</p>
+          <button 
+            className="bg-rose-500 hover:bg-rose-600 text-white p-1 border rounded-lg mt-2 px-3" 
+            onClick={() => setError(false)}
+          >
+              Ok
+          </button>
         </div>
-      </>}
-      {alert && 
-      <div className="pt-4 pb-5 mb-3 text-center bg-amber-100">
-        <p className="text-xl font-bold text-green-600">✓</p>
-        New order sent
-      </div>}
-      <button className="bg-rose-500 text-white p-2 w-full border rounded-lg disabled:bg-rose-400" onClick={handleCreateOrder} disabled={!client.trim() || total==0 ? true : false}>Send to kitchen</button>
+      ) : alert ? (
+        <div className="pt-4 pb-5 mb-3 text-center bg-amber-100">
+          <p className="text-xl font-bold text-green-600">✓</p>
+          New order sent
+        </div>
+      ) : (
+        <>
+          <div className="flex justify-between font-semibold text-base border-y border-dashed border-gray-600 py-1 my-5">
+            <span>Total:</span><span>${total}</span>
+          </div>
+          <div>
+            <input 
+              className="w-full border rounded placeholder:italic p-1 mb-4" 
+              type="text" id="customer" value={client} placeholder="*Customer's name" 
+              onChange={e => setClient(e.target.value)} 
+            />
+          </div>
+          <button 
+            className="bg-rose-500 hover:bg-rose-600 text-white p-2 w-full border rounded-lg disabled:text-gray-500 disabled:bg-gray-200" 
+            onClick={handleCreateOrder} disabled={!client.trim() || total==0 ? true : false}
+          >
+            Send to kitchen
+          </button>
+        </>
+      )}
     </div>
   );
 }
