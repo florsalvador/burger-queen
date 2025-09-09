88fb93c8f128c64aa5a36f929535ac3b59952641
florsalvador <florsalvador219@gmail.com>
2025-09-08 20:01:55 -0700

    feat(OrderCard): modify delete button so that only admins can delete orders

--- src/components/OrderCard.tsx
+++ src/components/OrderCard.tsx
@@ -73,12 +73,12 @@ function OrderCard({ order, onModify, onDeleteClick } : OrderCardProps) {
               <span className="flex justify-center gap-1">
                 <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" 
                   xmlns="http://www.w3.org/2000/svg" stroke="#ffffff">
-                  <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-                  <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                  <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                  <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
                   <g id="SVGRepo_iconCarrier"> 
                     <path 
-                      d="M4 12.6111L8.92308 17.5L20 6.5" stroke="#ffffff" stroke-width="2" 
-                      stroke-linecap="round" stroke-linejoin="round"
+                      d="M4 12.6111L8.92308 17.5L20 6.5" stroke="#ffffff" strokeWidth="2" 
+                      strokeLinecap="round" strokeLinejoin="round"
                     >
                     </path> 
                   </g>
@@ -96,12 +96,12 @@ function OrderCard({ order, onModify, onDeleteClick } : OrderCardProps) {
               <span className="flex justify-center gap-1">
                 <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" 
                   xmlns="http://www.w3.org/2000/svg" stroke="#ffffff">
-                  <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-                  <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                  <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                  <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
                   <g id="SVGRepo_iconCarrier"> 
                     <path 
-                      d="M4 12.6111L8.92308 17.5L20 6.5" stroke="#ffffff" stroke-width="2" 
-                      stroke-linecap="round" stroke-linejoin="round"
+                      d="M4 12.6111L8.92308 17.5L20 6.5" stroke="#ffffff" strokeWidth="2" 
+                      strokeLinecap="round" strokeLinejoin="round"
                     >
                     </path> 
                   </g>
@@ -111,14 +111,15 @@ function OrderCard({ order, onModify, onDeleteClick } : OrderCardProps) {
             </button>
           )}
       </div>
-
-      <button
-        className="absolute -top-2 -right-0 p-2
-          text-gray-400 hover:text-gray-700 font-bold"
-        onClick={() => onDeleteClick(order.id)}
-      >
-        ×
-      </button>
+      {session.user?.role === "admin" && (
+        <button
+          className="absolute -top-2 -right-0 p-2
+            text-gray-400 hover:text-gray-700 font-bold"
+          onClick={() => onDeleteClick(order.id)}
+        >
+          ×
+        </button>
+      )}
     </div>
   );
 }
