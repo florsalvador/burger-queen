b7352c5bd43f0ef8f4c6b9ba39083291c01f004b
florsalvador <florsalvador219@gmail.com>
2025-09-12 13:09:20 -0700

    refactor(Orders): add sidebar and header components

--- src/pages/Orders.tsx
+++ src/pages/Orders.tsx
@@ -1,4 +1,4 @@
-import { useNavigate, Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { getSession, clearSession } from "../services/authService";
 import { getOrders, modifyOrder, deleteOrder } from "../services/orderService";
@@ -6,6 +6,8 @@ import { Order } from "../models/Order";
 import OrderCard from "../components/OrderCard";
 import ConfirmModal from "../components/ConfirmModal";
 import MessageModal from "../components/MessageModal";
+import Header from "../components/Header";
+import Sidebar from "../components/Sidebar";
 
 function Orders() {
   const [orders, setOrders] = useState<Order[]>([]);
@@ -104,29 +106,9 @@ function Orders() {
 
   return (
     <div className="h-svh flex flex-col">
-      <header 
-        className="
-          flex justify-between p-4 sticky top-0 z-20
-          border-b-[1px] border-gray-400 box-border bg-white"
-      >
-        <h1 className="text-2xl font-bold">
-          🍔 <span className="text-gray-500">Burger</span><span className="text-amber-500">Queen</span>
-        </h1>
-        <p className="self-end">Welcome {session.user?.role}</p>
-      </header>
-      <main className="flex flex-1 overflow-hidden">
-        <div className="w-[15%] flex flex-col bg-gray-100 font-semibold text-gray-600 relative">
-          <div className="pt-4">
-            <Link to="/" className="block bg-gray-100 py-3 pl-6">Menu</Link>
-            <Link to="/orders" className="block bg-white py-3 pl-6">Order List</Link>
-          </div>
-          <button 
-            className="absolute bottom-8 left-1/2 -translate-x-1/2" 
-            data-testid="logoutBtn" onClick={handleLogout}
-          >
-            {"<"} Log out
-          </button>
-        </div>
+      <Header role={session.user?.role ?? null} />
+      <main className="flex flex-1 overflow-hidden bg-gray-800">
+        <Sidebar activePage="orders" onClickLogout={handleLogout} />
         <div className="flex-1 p-4 overflow-y-auto">
           <div className="flex border-b mb-4">
             <button
